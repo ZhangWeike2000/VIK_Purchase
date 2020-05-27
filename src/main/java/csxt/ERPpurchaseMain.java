41a406eee8098addac8775077d83e686afcb1710
Admin <814555725@qq.com>
2020-05-27 11:20:04 +0800

    jyx

---
+++ src/main/java/csxt/ERPpurchaseMain.java
@@ -0,0 +1,11 @@
+package csxt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ERPpurchaseMain {
+    public static void main(String[] args){
+        SpringApplication.run(ERPpurchaseMain.class,args);
+    }
+}
